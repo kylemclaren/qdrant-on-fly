3138f0cd3cb3c61e1bc618afb6b0a675122086de
Kyle McLaren <kylemclaren@protonmail.com>
2023-05-29 16:51:07 +0200

    delete old scripts

--- start-seed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TAILSCALE_HOSTNAME="qdrant-seed"
-
-/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${TAILSCALE_HOSTNAME}"
-
-./qdrant --uri "http://${TAILSCALE_HOSTNAME}.${TAILNET_DOMAIN}:6335"
