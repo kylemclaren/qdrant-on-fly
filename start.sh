68efa465212b4db7a7251f5b1bcaf13e4b6712b8
Kyle McLaren <mclaren@fly.io>
2023-06-21 16:57:07 +0200

    hardening

--- start.sh
+++ start.sh
@@ -1,20 +1,27 @@
 #! /bin/bash
+
 set -euo pipefail
 
-host=$FLY_MACHINE_ID.vm.$FLY_APP_NAME.internal
+host="${FLY_MACHINE_ID}.vm.${FLY_APP_NAME}.internal"
 
-this_host=$(getent ahostsv6 fly-local-6pn | head -1 | cut -d ' ' -f1)
+# Get current host IP
+host_ipv6=$(getent ahostsv6 fly-local-6pn | head -n1 | awk '{print $1}')
+if [[ -z "${host_ipv6}" ]]; then
+    echo "Error: Unable to determine this host's IP address."
+    exit 1
+fi
+
+# Determine seed node
 seed=
-
-for i in $(dig +short aaaa $FLY_APP_NAME.internal)
-do
-    if [[ "$i" != "$this_host" ]]; then
-	seed=$i
-	break
+for i in $(dig +short aaaa "${FLY_APP_NAME}.internal"); do
+    if [[ "${i}" != "${host_ipv6}" ]]; then
+        seed="${i}"
+        break
     fi
 done
 
-if [[ "$seed" != "" ]]; then
+# Start qdrant
+if [[ -n "${seed}" ]]; then
     # If there are other active devices, use the HostName of one of them.
     echo "Bootstrapping new peer..."
     ./qdrant --bootstrap "http://[${seed}]:6335" --uri "http://${host}:6335"
